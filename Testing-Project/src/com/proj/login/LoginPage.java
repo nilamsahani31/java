286619c81e0cbeaad8d7c45790d4b4310d79c56f
nilamsahani <nilamsahani2001@gmail.com>
2024-04-13 11:54:46 +0530

    task complete

---
+++ Testing-Project/src/com/proj/login/LoginPage.java
@@ -0,0 +1,15 @@
+package com.proj.login;
+
+public class LoginPage {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Add Tew Number :");
+		
+		int a= 10;
+		int b= 20;
+		
+		System.out.println(a+b);
+	}
+	
+}
